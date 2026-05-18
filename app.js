a7300fdca94d6a069ad54417ef35d7becc634100
allysafgorduiz9-lab <allysafgorduiz9@gmail.com>
2026-05-18 14:50:50 +0800

    auto-commit

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'simeon_brewers',
-    port: process.env.DB_PORT || 3306,
+    database: process.env.DB_NAME || 'defaultdb',
+    port: process.env.DB_PORT || 23249,
     ssl: { rejectUnauthorized: false } 
 });
 
